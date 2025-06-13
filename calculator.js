9a08f02d0e9a696f774f4e27f80b2cbac3bcd096
Erharhine Donald <donerharhine@gmail.com>
2025-06-13 07:30:25 +0100

    assignment done

--- calculator.js
+++ calculator.js
@@ -1,3 +1,10 @@
+
+const result = document.getElementById('calculation-result');
+const addBtn = document.getElementById('add')
+const subtractBtn = document.getElementById('subtract')
+const divideBtn = document.getElementById('divide')
+const multiplyBtn = document.getElementById('multiply')
+
 const add = (num1, num2) => {
     return num1 + num2;
 };
@@ -7,16 +14,35 @@ const subtract = (num1, num2) => {
 };
 
 const multiply = (a, b) => {
-    return a * b;
+    return  a * b;
 };
 
 const divide = (a, b) => {
     return a / b;
 };
 
+const getInputs = () => {
+    const input1 = parseFloat(document.getElementById('number1').value) || 0;
+    const input2 = parseFloat(document.getElementById('number2').value) || 0; 
+    return [ input1, input2 ]
+}
 
+addBtn.addEventListener('click', () => { 
+    const [ input1, input2 ] = getInputs()
+    result.textContent = add(input1, input2);   
+})
 
-const addBtn = document.getElementById('add')
-addBtn.addEventListener('click', () => {
-    add()
+multiplyBtn.addEventListener('click', () => { 
+    const [ input1, input2 ] = getInputs()
+    result.textContent = multiply(input1, input2);   
+})
+
+divideBtn.addEventListener('click', () => { 
+    const [ input1, input2 ] = getInputs()
+    result.textContent = divide(input1, input2);   
+})
+
+subtractBtn.addEventListener('click', () => { 
+    const [ input1, input2 ] = getInputs()
+    result.textContent = subtract(input1, input2);   
 })
